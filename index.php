e329ac2d679934f06c1dfcf5fb0de07241271181
Johann Schopplich <mail@johannschopplich.com>
2021-08-16 20:17:37 +0200

    refactor: remove unused namespaces

--- index.php
+++ index.php
@@ -1,12 +1,7 @@
 <?php
 
-use Kirby\Cms\App;
-use Kirby\Cms\Blueprint;
-use Kirby\Toolkit\F;
-use Kirby\Toolkit\Str;
-
-Kirby::plugin('rasteiner/k3-pagesdisplay-section', [
+\Kirby\Cms\App::plugin('rasteiner/k3-pagesdisplay-section', [
     'sections' => [
-        'pagesdisplay' => require __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'PagesDisplaySection.php'
+        'pagesdisplay' => require __DIR__ . '/src/PagesDisplaySection.php'
     ]
 ]);
